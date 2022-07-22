345f8814b282245056d2d188834ae0a40d8908ef
Andre Hui <andrehui09@gmail.com>
2022-07-22 16:45:28 -0700

    rethemed, cut down a lot of fluff

--- js/resume.js
+++ js/resume.js
@@ -1,4 +1,46 @@
-(function($) {
+var targetTxt = 'ANDRE HUI';
+var alphabetSequence = '-ABCDEFGHIJKLMNOPQRSTUVWXYZ ';
+
+function loadName() {
+    var currName = document.getElementById("name").innerHTML;
+    if (currName != targetTxt) {
+        for (let i = 0; i < targetTxt.length; i++) {
+            if (currName.charAt(i) != targetTxt.charAt(i)) {
+                var alphaIx = alphabetSequence.indexOf(currName.charAt(i));
+                var targAlphaIx = alphabetSequence.indexOf(targetTxt.charAt(i));
+
+                var plusDist = 0;
+                var minusDist = 0;
+
+                if (targAlphaIx > alphaIx) {
+                    plusDist = targAlphaIx - alphaIx;
+                    minusDist = alphaIx + alphabetSequence.length - targAlphaIx;
+                } else {
+                    plusDist = targAlphaIx - (alphaIx - alphabetSequence.length);
+                    minusDist = alphaIx - targAlphaIx;
+                }
+
+                if (minusDist < plusDist) {
+                    alphaIx--;
+                    if (alphaIx < 0) {
+                        alphaIx += alphabetSequence.length;
+                    }
+                } else {
+                    alphaIx++;
+                }
+                document.getElementById("name").innerHTML = currName.substring(0, i) + alphabetSequence.charAt(alphaIx) + currName.substring(i + 1, targetTxt.length);
+                break;
+            }
+        }
+    }
+    setTimeout(loadName, 65);
+};
+
+$(document).ready(function () {
+    setTimeout(loadName, 200)
+});
+
+(function ($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
@@ -24,5 +66,7 @@
   $('body').scrollspy({
     target: '#sideNav'
   });
+
+    
 
 })(jQuery); // End of use strict
